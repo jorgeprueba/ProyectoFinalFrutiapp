4d826a5a66e12d2a6ba2f41d85cbd6ca2117bdbe
jorge <jlgo60@hotmail.com>
2020-08-23 06:04:50 -0500

    Se agrego incono frutita

--- app/src/main/java/com/example/frutiapp/MainActivity.kt
+++ app/src/main/java/com/example/frutiapp/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//Metodo para colocar icono en el actionbar
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setIcon(R.drawable.frutita)
+
     }
 }
