9b1b157b167fce2c263a5b3ba69b711e8c0ec30e
jorge <jlgo60@hotmail.com>
2020-08-25 05:33:03 -0500

    Se agrego cancion,icono y toast

--- app/src/main/java/com/example/frutiapp/MainActivity2Nivel1.kt
+++ app/src/main/java/com/example/frutiapp/MainActivity2Nivel1.kt
@@ -1,17 +1,53 @@
 package com.example.frutiapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main_activity2_nivel1.*
 
 class MainActivity2Nivel1 : AppCompatActivity() {
+    var score = 0
+    var numAleatorio_uno: Int = 0
+    var numAleatorio_dos: Int = 0
+    var resultado: Int = 0
+    var vidas: Int = 3
+
+    //var nombre_jugador: String? = null
+    var string_score: kotlin.String? = null
+    var string_vidas: kotlin.String? = null
+
+    lateinit var mp: MediaPlayer
+    lateinit var mp_great: MediaPlayer
+    lateinit var mp_bad: MediaPlayer
+
+    val numero = arrayOf(
+        "cero", "uno", "dos", "tres", "cuatro", "cinco", "seis", "siete", "ocho", "nueve"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_activity2_nivel1)
 
         val intent = intent
         val name = intent.getStringExtra("Clave")
-        textViewNombre.text = name
+        textViewNombre.text = ("Jugador: $name")
+
+        //Metodo para mostrar icono en acction bar
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.setIcon(R.mipmap.ic_launcher)
+
+        // Metodo para iniciar cancion de bienvenida
+        mp = MediaPlayer.create(this, R.raw.goats)
+        mp.start()
+        mp.isLooping
+
+        //Metodo para indicar sonido great y bad
+        mp_great = MediaPlayer.create(this,R.raw.wonderful)
+        mp_bad = MediaPlayer.create(this,R.raw.bad)
+
+
+        Toast.makeText(this, "Nivel 1 - Sumas basicas", Toast.LENGTH_SHORT).show()
 
     }
 }
